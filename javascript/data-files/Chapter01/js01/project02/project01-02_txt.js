8869de74f712e11b3752341ecf631df4c63ffbfc
jesseshults <jesse_shults@insideranken.org>
2025-10-25 13:36:28 -0500

    hands on 1-2

--- javascript/data-files/Chapter01/js01/project02/project01-02_txt.js
+++ javascript/data-files/Chapter01/js01/project02/project01-02_txt.js
@@ -2,10 +2,32 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Jesse Shults
+    Date: 10/25/25  
 
     Filename: project01-02.js
 */
 
-//define variables for service name and service speed
+// Define variables for service name and service speed
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+
+let service1Speed = "50 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
+
+// Output values into the HTML table
+document.getElementById("service1").textContent = service1Name;
+document.getElementById("speed1").textContent = service1Speed;
+
+document.getElementById("service2").textContent = service2Name;
+document.getElementById("speed2").textContent = service2Speed;
+
+document.getElementById("service3").textContent = service3Name;
+document.getElementById("speed3").textContent = service3Speed;
+
+document.getElementById("service4").textContent = service4Name;
+document.getElementById("speed4").textContent = service4Speed;
