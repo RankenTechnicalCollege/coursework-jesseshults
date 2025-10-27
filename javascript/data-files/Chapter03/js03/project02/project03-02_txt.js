68902aef86df9e19fb543ab924d101e0d63aa058
jesseshults <jesse_shults@insideranken.org>
2025-10-27 15:48:53 -0500

    ch3p2

--- javascript/data-files/Chapter03/js03/project02/project03-02_txt.js
+++ javascript/data-files/Chapter03/js03/project02/project03-02_txt.js
@@ -1,28 +1,43 @@
-/*    JavaScript 7th Edition
-      Chapter 3
-      Project 03-02
+/*
+   JavaScript 7th Edition
+   Chapter 3
+   Project 03-02
 
-      Application to generate a slide gallery
-      Author: 
-      Date:   
+   Application to display a slide gallery
+   Author: Jesse Shults
+   Date:   10/27/25
 
-      Filename: project03-02.js
+   Filename: project03-02.js
 */
 
-let captions = new Array(14);
-captions[0]="International Space Station fourth expansion [2009]";
-captions[1]="Assembling the International Space Station [1998]";
-captions[2]="The Atlantis docks with the ISS [2001]"; 
-captions[3]="The Atlantis approaches the ISS [2000]"; 
-captions[4]="The Atlantis approaches the ISS [2000]"; 
-captions[5]="International Space Station over Earth [2002]";
-captions[6]="The International Space Station first expansion [2002]";
-captions[7]="Hurricane Ivan from the ISS [2008]";
-captions[8]="The Soyuz spacecraft approaches the ISS [2005]";
-captions[9]="The International Space Station from above [2006]";
-captions[10]="Maneuvering in space with the Canadarm2 [2006]";
-captions[11]="The International Space Station second expansion [2006]";
-captions[12]="The International Space Station third expansion [2007]";
-captions[13]="The ISS over the Ionian Sea [2007]";
+// Captions for the 14 images
+let captions = [
+  "International Space Station fourth expansion (2009)",
+  "Assembling the International Space Station (1998)",
+  "The Atlantis docks with the ISS (2001)",
+  "The Atlantis approaches the ISS (2000)",
+  "The Atlantis approaches the ISS (2000)",
+  "International Space Station over Earth (2002)",
+  "The International Space Station first expansion (2002)",
+  "Hurricane Ivan from the ISS (2008)",
+  "The Soyuz spacecraft approaches the ISS (2005)",
+  "The International Space Station from above (2006)",
+  "Maneuvering in space with the Canadarm2",
+  "The International Space Station second expansion (2006)",
+  "The International Space Station third expansion (2007)",
+  
+];
 
+// Start with an empty string for gallery HTML
+let htmlCode = "";
 
+// Generate HTML for each image and caption
+for (let i = 0; i < captions.length; i++) {
+  htmlCode += "<figure>";
+  htmlCode += `<img src='slide${i + 1}.jpg' alt=''>`;
+  htmlCode += `<figcaption>${captions[i]}</figcaption>`;
+  htmlCode += "</figure>";
+}
+
+// Output the gallery into the page
+document.getElementById("gallery").innerHTML = htmlCode;
